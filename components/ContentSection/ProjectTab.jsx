557bb99ac36bf15c397814018c3761b3ea262b2a
Daniel Ward <50274655+Quarantiine@users.noreply.github.com>
2023-02-02 10:48:59 -0600

    sm changes

--- components/ContentSection/ProjectTab.jsx
+++ components/ContentSection/ProjectTab.jsx
@@ -76,9 +76,6 @@ const Project = ({ project, index }) => {
 						<Link href={project.link} target={`_blank`}>
 							<button className="base-bg-1 px-2 py-1 text-md rounded-sm hover:opacity-60">Website</button>
 						</Link>
-						<button className="border border-white hover:bg-[#0E51FF] hover:border-transparent transition px-2 py-1 text-md rounded-sm">
-							Preview
-						</button>
 					</div>
 				</div>
 			</div>
